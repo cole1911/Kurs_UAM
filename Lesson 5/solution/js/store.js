bdf98700eafc91285c18f3b3e7cb89540386c748
Damian <damian.jozefowski@gmail.com>
2014-12-08 19:20:17 +0100

    Add protorype function in Store

--- Lesson 5/solution/js/store.js
+++ Lesson 5/solution/js/store.js
@@ -2,18 +2,6 @@ UAM.Store = function () {
 	UAM.EventEmitter.call(this);
 	this.data  = [];
 	this.selectedItems = [];
-
-	//true gdy element zostal dodany, false gdy usuniety
-	this.checkItem = function(item) {
-		if(this.selectedItems.indexOf(item) < 0) {
-			this.selectedItems.push(item);
-			return true;
-		} else {
-			this.selectedItems.splice(this.selectedItems.indexOf(item),1);
-			return false;
-		}
-	};
-	this.checkItem.bind(this);
 };
 
 UAM.utils.inherits(UAM.EventEmitter, UAM.Store);
@@ -26,4 +14,13 @@ UAM.Store.prototype.updateFooter = function (el) {
 	var isElementActive = this.checkItem(el);
 	this.emit("elementChecked",isElementActive,el);
 	this.emit("updateFooterView");
+};
+UAM.Store.prototype.checkItem = function(item) {
+	if(this.selectedItems.indexOf(item) < 0) {
+		this.selectedItems.push(item);
+		return true;
+	} else {
+		this.selectedItems.splice(this.selectedItems.indexOf(item),1);
+		return false;
+	}
 };
